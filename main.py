30cc5c2e888b87b3af5986ff3f7220ddea7e8621
ThiagoSilva20 <thiago201714@gmail.com>
2023-04-16 22:22:47 -0300

    som

--- main.py
+++ main.py
@@ -5,21 +5,40 @@ from random import randint
 
 pygame.init()
 
-largura = 640
-altura = 480
+pygame.mixer.music.set_volume(0.2)
+musica_de_fundo = pygame.mixer.music.load('musicadefundo.mp3')
+pygame.mixer.music.play(-1) #-1 para repetir a musica
+
+barulho_colisao = pygame.mixer.Sound('colisao.wav')
+
+
+largura = 700
+altura = 540
+
 y = altura / 2 - 50/2
 x = largura/2 - 40/2
 
-x_azul = randint(40,600)
-y_azul = randint(50,430)
+x_azul = randint(40, 660)
+y_azul = randint(50, 490)
+
+pontos = 0
+fonte = pygame.font.SysFont('arial', 40, True, True)
+
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
 relogio = pygame.time.Clock()
 
 while True:
+
     relogio.tick(60)
+    
     tela.fill((0,0,0))
+    
+    mensagem = f'Pontos: {pontos}'
+
+    texto_formatado = fonte.render(mensagem, False, (255, 255, 255))
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -45,11 +64,15 @@ while True:
         y = y + 4
 
     ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x, y, 40, 50))
+   
     ret_azul = pygame.draw.rect(tela, (0,0,255), (x_azul, y_azul, 40, 50))
 
     if ret_vermelho.colliderect(ret_azul):
-        x_azul = randint(40, 600)
-        y_azul = randint(50, 430)
+        x_azul = randint(40, 660)
+        y_azul = randint(50, 500)
+        pontos = pontos + 1
+        barulho_colisao.play()
 
-    """test"""
+    tela.blit(texto_formatado, (490, 5))
+    
     pygame.display.update()
